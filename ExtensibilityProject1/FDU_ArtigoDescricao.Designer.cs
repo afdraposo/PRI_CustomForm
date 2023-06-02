953707d1e5394e69c8769b3c9f210ff7f6d1628f
André Raposo <araposo@weba.pt>
2023-06-02 11:17:19 +0000

    v1.0

---
+++ ExtensibilityProject1/FDU_ArtigoDescricao.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ExtensibilityProject1
+{
+    partial class FDU_ArtigoDescricao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_codigoArtigo = new System.Windows.Forms.TextBox();
+            this.tb_descricao = new System.Windows.Forms.TextBox();
+            this.label_codigoArtigo = new System.Windows.Forms.Label();
+            this.label_descricao = new System.Windows.Forms.Label();
+            this.btn_verificar = new System.Windows.Forms.Button();
+            this.btn_inserir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_codigoArtigo
+            // 
+            this.tb_codigoArtigo.Location = new System.Drawing.Point(173, 44);
+            this.tb_codigoArtigo.Name = "tb_codigoArtigo";
+            this.tb_codigoArtigo.Size = new System.Drawing.Size(298, 20);
+            this.tb_codigoArtigo.TabIndex = 0;
+            // 
+            // tb_descricao
+            // 
+            this.tb_descricao.Location = new System.Drawing.Point(173, 91);
+            this.tb_descricao.Multiline = true;
+            this.tb_descricao.Name = "tb_descricao";
+            this.tb_descricao.Size = new System.Drawing.Size(298, 98);
+            this.tb_descricao.TabIndex = 1;
+            // 
+            // label_codigoArtigo
+            // 
+            this.label_codigoArtigo.AutoSize = true;
+            this.label_codigoArtigo.Location = new System.Drawing.Point(79, 47);
+            this.label_codigoArtigo.Name = "label_codigoArtigo";
+            this.label_codigoArtigo.Size = new System.Drawing.Size(88, 13);
+            this.label_codigoArtigo.TabIndex = 2;
+            this.label_codigoArtigo.Text = "Código de Artigo:";
+            // 
+            // label_descricao
+            // 
+            this.label_descricao.AutoSize = true;
+            this.label_descricao.Location = new System.Drawing.Point(109, 94);
+            this.label_descricao.Name = "label_descricao";
+            this.label_descricao.Size = new System.Drawing.Size(58, 13);
+            this.label_descricao.TabIndex = 3;
+            this.label_descricao.Text = "Descrição:";
+            // 
+            // btn_verificar
+            // 
+            this.btn_verificar.Location = new System.Drawing.Point(519, 40);
+            this.btn_verificar.Name = "btn_verificar";
+            this.btn_verificar.Size = new System.Drawing.Size(75, 23);
+            this.btn_verificar.TabIndex = 4;
+            this.btn_verificar.Text = "Verificar";
+            this.btn_verificar.UseVisualStyleBackColor = true;
+            this.btn_verificar.Click += new System.EventHandler(this.btn_verificar_Click);
+            // 
+            // btn_inserir
+            // 
+            this.btn_inserir.Location = new System.Drawing.Point(519, 94);
+            this.btn_inserir.Name = "btn_inserir";
+            this.btn_inserir.Size = new System.Drawing.Size(75, 23);
+            this.btn_inserir.TabIndex = 5;
+            this.btn_inserir.Text = "Inserir";
+            this.btn_inserir.UseVisualStyleBackColor = true;
+            this.btn_inserir.Click += new System.EventHandler(this.btn_inserir_Click);
+            // 
+            // PriCustomForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btn_inserir);
+            this.Controls.Add(this.btn_verificar);
+            this.Controls.Add(this.label_descricao);
+            this.Controls.Add(this.label_codigoArtigo);
+            this.Controls.Add(this.tb_descricao);
+            this.Controls.Add(this.tb_codigoArtigo);
+            this.Name = "PriCustomForm1";
+            this.Size = new System.Drawing.Size(668, 242);
+            this.Text = "PriCustomForm1";
+            this.Load += new System.EventHandler(this.PriCustomForm1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_codigoArtigo;
+        private System.Windows.Forms.TextBox tb_descricao;
+        private System.Windows.Forms.Label label_codigoArtigo;
+        private System.Windows.Forms.Label label_descricao;
+        private System.Windows.Forms.Button btn_verificar;
+        private System.Windows.Forms.Button btn_inserir;
+    }
+}
---
+++ ExtensibilityProject1/FDU_ArtigoDescricao.cs
@@ -0,0 +1,104 @@
+﻿using Primavera.Extensibility.CustomForm;
+using System;
+using System.Data.SqlClient;
+
+namespace ExtensibilityProject1
+{
+    public partial class FDU_ArtigoDescricao : CustomForm
+    {
+        private string _codigoArtigo;
+
+        public FDU_ArtigoDescricao()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_verificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(tb_codigoArtigo.Text))
+                {
+                    string connStr = this.PSO.BaseDados.DaConnectionStringNET(this.PSO.BaseDados.DaNomeBDdaEmpresa(this.BSO.Contexto.CodEmp), this.BSO.Contexto.Instancia);
+
+                    using (SqlConnection con = new SqlConnection(connStr))
+                    {
+                        string query = "SELECT * from Artigo " +
+                                       $"where Artigo = '{tb_codigoArtigo.Text}'";
+
+                        SqlCommand selectCommand = new SqlCommand(query, con);
+
+                        con.Open();
+
+                        using (SqlDataReader reader = selectCommand.ExecuteReader())
+                        {
+
+                            if (reader.Read())
+                            {
+                                tb_descricao.Enabled = true;
+                                btn_inserir.Enabled = true;
+                                _codigoArtigo = reader["Artigo"].ToString();
+                                tb_descricao.Text = reader["Descricao"].ToString();
+                            }
+                            else
+                            {
+                                PSO.Dialogos.MostraAviso("Código de Artigo não existe!", StdPlatBS100.StdBSTipos.IconId.PRI_Exclama);
+                            }
+                        }
+                        con.Close();
+                    }
+                }
+                else
+                {
+                    PSO.Dialogos.MostraAviso("Campo do Código de Artigo Vazio!", StdPlatBS100.StdBSTipos.IconId.PRI_Exclama);
+                }
+            }
+            catch (Exception ex)
+            {
+                PSO.Dialogos.MostraAviso("Error: " + ex.Message, StdPlatBS100.StdBSTipos.IconId.PRI_Critico);
+            }
+        }
+
+        private void btn_inserir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(tb_descricao.Text))
+                {
+                    string connStr = this.PSO.BaseDados.DaConnectionStringNET(this.PSO.BaseDados.DaNomeBDdaEmpresa(this.BSO.Contexto.CodEmp), this.BSO.Contexto.Instancia);
+
+                    using (SqlConnection con = new SqlConnection(connStr))
+                    {
+                        con.Open();
+
+                        SqlCommand updateCommand = con.CreateCommand();
+
+                        updateCommand.CommandText = "UPDATE Artigo " +
+                            $"set Descricao = '{tb_descricao.Text}' " +
+                            $"where Artigo = '{_codigoArtigo}'";
+
+                        updateCommand.ExecuteNonQuery();
+
+                        con.Close();
+                    }
+
+                    PSO.Dialogos.MostraAviso("Descrição editada com sucesso!", StdPlatBS100.StdBSTipos.IconId.PRI_Informativo);
+                }
+                else
+                {
+                    PSO.Dialogos.MostraAviso("Campo da Descrição Vazio!", StdPlatBS100.StdBSTipos.IconId.PRI_Exclama);
+                }
+            }
+            catch (Exception ex)
+            {
+                PSO.Dialogos.MostraAviso("Error: " + ex.Message, StdPlatBS100.StdBSTipos.IconId.PRI_Critico);
+            }
+        }
+
+        private void PriCustomForm1_Load(object sender, EventArgs e)
+        {
+            tb_descricao.Enabled = false;
+            btn_inserir.Enabled = false;
+        }
+    }
+}
